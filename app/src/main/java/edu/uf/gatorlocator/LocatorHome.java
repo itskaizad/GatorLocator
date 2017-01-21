146cfd86adfc945fee47f22083766dc8fb48130f
Kaizad Avari <kaizad.avari@live.com>
2017-01-21 03:54:13 -0500

    Initial commit

---
+++ app/src/main/java/edu/uf/gatorlocator/LocatorHome.java
@@ -0,0 +1,20 @@
+package edu.uf.gatorlocator;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rodolfonavalon.shaperipplelibrary.ShapeRipple;
+
+public class LocatorHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locator_home);
+
+//        ShapeRipple ripple = (ShapeRipple) findViewById(R.id.ripple);
+//        ripple.setRippleColor(Color.BLUE);
+//        ripple.setRippleDuration(2200);
+    }
+}
